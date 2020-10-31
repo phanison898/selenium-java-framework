87d3cec690320456611ed2eb878a4936141de9bd
Phanison <Phanison@DESKTOP-6VKSR66>
2020-10-31 15:33:49 +0530

    updated extent config

--- Automation-Framework/src/main/java/com/qa/report/ExtentManager.java
+++ Automation-Framework/src/main/java/com/qa/report/ExtentManager.java
@@ -1,5 +1,8 @@
 package com.qa.report;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
@@ -22,10 +25,27 @@ public class ExtentManager extends Config{
 	}
 	
 	private void extentConfig() {
-		extent.setSystemInfo("Platform", System.getProperty("os.name"));
-		extent.setSystemInfo("Java -version", System.getProperty("java.version"));
+		
+		InetAddress ip = null;
+		try {
+			ip = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		String host_name = ip.getHostName();
+		String host_address = ip.getHostAddress();
+		String app_url = String.format("<a href=%s target=_blank>%<s</a>", TEST_CONFIG("Application-URL"));
+		String email = String.format("<a href=mailto:%s target=_blank>%<s</a>", REPORT_CONFIG("Report-Author-Email"));
+		
+		extent.setSystemInfo("Application", app_url);
+		extent.setSystemInfo("Browser", TEST_CONFIG("Browser").toUpperCase());
+		extent.setSystemInfo("Host-Name", host_name);
+		extent.setSystemInfo("Host-Address", host_address);
+		extent.setSystemInfo("Host-Platform", System.getProperty("os.name"));
+		extent.setSystemInfo("Host Java -v", System.getProperty("java.version"));
 		extent.setSystemInfo("Author", REPORT_CONFIG("Report-Author"));
-		extent.setSystemInfo("Email", REPORT_CONFIG("Report-Author-Email"));
+		extent.setSystemInfo("Email", email);
 	}
 	
 	private void sparkConfig() {
